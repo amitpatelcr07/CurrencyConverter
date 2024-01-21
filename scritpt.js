323a4ae0396fdadd2e9f2ba0222af67d19235a09
amitpatelcr07 <amitcoder07@gmail.com>
2024-01-21 22:44:10 +0530

    some basic function added in js

--- scritpt.js
+++ scritpt.js
@@ -1,10 +1,49 @@
 let change =document.querySelector(".img");
 let rselect=document.querySelector(".rightCur");
 let lselect=document.querySelector(".leftCur");
+let convert=document.querySelector(".convert");
+let display=document.querySelector(".dis");
+let currency={
+    INR: 85,
+    Japan:34,
+    UK:12,
+    US:1
+}
 
+let money;
 let swap=()=>{
-    console.log("Left Value"+ rselect.value);
-    console.log("Right Value"+ lselect.value);
+   if(lselect.value!=rselect.value){
+    let left=lselect.value
+    let right=rselect.value;
+    rselect.value=left;
+    lselect.value=right;
+    // console.log("Left Value"+ rselect.value);
+    // console.log("Right Value"+ lselect.value);
+   }else{
+    alert("Both currency should not be same");
+   }
+
 }
 
 change.addEventListener("click",swap);
+
+
+let curConvert=()=>{
+  Object.entries(currency).forEach(([key,val])=>{
+    if(key===lselect.value){
+        console.log(val);
+        console.log(lselect.value);
+        // money=((currency.rselect.value)/(currencylselect.value))*2;
+         display.innerHTML=`2 ${lselect.value} is ${23} ${rselect.value}`;
+    }
+  })
+}
+
+convert.addEventListener("click",curConvert);
+
+
+
+
+
+  
+  
